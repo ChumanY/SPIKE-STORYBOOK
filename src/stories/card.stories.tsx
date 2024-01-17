ee4964a56695cc55def56dd9ab4d1f57fa09962c
ChumanY <cuerpoespin_2000@hotmail.com>
2024-01-17 16:38:20 -0500

    v13

--- src/stories/card.stories.tsx
+++ src/stories/card.stories.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import Card, { CardProps } from '../components/card/card';
 
-const githubUsername = process.env.GITHUB_ACTOR || 'defaultUsername'; 
+
+const githubUsername = process.env.GITHUB_ACTOR; 
 
 export default {
   title: 'Card',
   component: Card,
-  parameters: {
-    githubActor: githubUsername,
-  },
 } as Meta;
+
 
 const Template: Story<CardProps> = (args) => <Card {...args} />;
 
 export const CardComponent = Template.bind({});
 CardComponent.args = {
-  title: `${githubUsername}'s Card`,
+  title: `${githubUsername}'s Card`, 
   content: 'This is the content of the card.',
 };
