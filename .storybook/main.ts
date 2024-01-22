7a4f478396907601555d4a7e323901947b1573f0
ChumanY <cuerpoespin_2000@hotmail.com>
2024-01-22 10:30:54 -0500

    V1.1

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,17 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+// .storybook/main.js
+const { execSync } = require('child_process');
 
-const config: StorybookConfig = {
+// Obtener el nombre del autor del último commit usando un comando de git
+const getAuthorName = () => {
+  try {
+    return execSync('git log -1 --pretty=format:%an').toString().trim();
+  } catch (error) {
+    console.error('Error obteniendo el nombre del autor:', error.message);
+    return 'unknown-author';
+  }
+};
+
+module.exports = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
@@ -15,5 +26,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  // Utilizar el nombre del autor en el path de salida
+  outputDir: `../build/${getAuthorName()}/storybook`,
 };
-export default config;
