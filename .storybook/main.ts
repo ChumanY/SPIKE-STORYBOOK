86fe5c1acb394f3367ff89197a72fa1d7b2330a1
ChumanY <cuerpoespin_2000@hotmail.com>
2024-01-22 17:57:55 -0500

    v1.4

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,10 @@
 // .storybook/main.js
 const { execSync } = require('child_process');
 
-// Obtener el nombre del autor del último commit usando un comando de git
 const getAuthorName = () => {
   try {
     const authorName = execSync('git log -1 --pretty=format:%an').toString().trim();
-    console.log('Nombre del autor:', authorName); // Agregar log aquí
+    console.log('Nombre del autor:', authorName); 
     return authorName;
   } catch (error) {
     console.error('Error obteniendo el nombre del autor:', error.message);
@@ -28,6 +27,5 @@ module.exports = {
   docs: {
     autodocs: "tag",
   },
-  // Utilizar el nombre del autor en el path de salida
   outputDir: `../build/${getAuthorName()}/storybook`,
 };
